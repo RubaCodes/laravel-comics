be8f895674927b70f9b75e71f1f8193706381dbd
rubacodes <rubacodes@gmail.com>
2022-07-07 15:49:43 +0200

    fix immagini socials

--- resources/views/partials/footer__top.blade.php
+++ resources/views/partials/footer__top.blade.php
@@ -6,19 +6,19 @@
       <div class="footer__bot--right">
         <span>follow us</span>
         <a href="#"
-          ><img src="../../assets/img/footer-facebook.png" alt="facebook" />
+          ><img src="{{asset('images/footer-facebook.png')}}"  alt="facebook" />
         </a>
         <a href="#">
-          <img src="../../assets/img/footer-twitter.png" alt="twitter" />
+          <img src="{{asset('images/footer-twitter.png')}}" alt="twitter" />
         </a>
         <a href="#"
-          ><img src="../../assets/img/footer-youtube.png" alt="youtube" />
+          ><img src="{{asset('images/footer-youtube.png')}}" alt="youtube" />
         </a>
         <a href="#"
-          ><img src="../../assets/img/footer-periscope.png" alt="periscope" />
+          ><img src="{{asset('images/footer-periscope.png')}}" alt="periscope" />
         </a>
         <a href="#">
-          <img src="../../assets/img/footer-pinterest.png" alt="pinterest" />
+          <img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest" />
         </a>
       </div>
     </div>
