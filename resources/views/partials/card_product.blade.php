63f34e194f84ca4e73dfdf03ca0fac633906d3dd
rubacodes <rubacodes@gmail.com>
2022-07-07 18:40:06 +0200

    push del test

--- resources/views/partials/card_product.blade.php
+++ resources/views/partials/card_product.blade.php
@@ -1,6 +1,4 @@
   <div class="card">
-    <img src="@yield('thumbnail')" alt="@yield('titolo_comic')" />
-    <h3> @yield('titolo_comic') </h3>   
+      <img src="@yield('thumbnail')" alt="@yield('titolo_comic')" />
+      <h3> @yield('titolo_comic') </h3>
   </div>
-
-   {{-- se provo a ciclare piu blade components non va --}}
--- resources/views/partials/shop_library.blade.php
+++ resources/views/partials/shop_library.blade.php
@@ -2,12 +2,20 @@
     <div class="container-lg">
         <h2>current series</h2>
         <ul>
+
             @foreach ($comics as $comic)
                 <li>
                     <div class="card">
                         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" />
                         <h3> {{ $comic['title'] }} </h3>
                     </div>
+                    {{-- @extends('partials.card_product')
+                    @section('thumbnail')
+                        {{ $comic['thumb'] }}
+                    @endsection
+                    @section('titolo_comic')
+                        {{ $comic['title'] }}
+                    @endsection --}}
                 </li>
             @endforeach
         </ul>
