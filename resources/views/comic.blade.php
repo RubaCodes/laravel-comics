bd6bebbd682cabc73584f9178a659ca5a9c76719
rubacodes <rubacodes@gmail.com>
2022-07-08 17:09:31 +0200

    ri push del commit

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -26,8 +26,47 @@
                 <img src="{{ asset('/images/adv.jpg') }}" alt="adv">
             </div>
         </div>
-        <div class="comic-info__bottom container-lg">
-
+        <div class="comic-info__bottom ">
+            <div class=" boh container-lg">
+                <div class="talent">
+                    <h3>Talent</h3>
+                    <ul>
+                        <li>
+                            <span>Art by:</span>
+                            <p>
+                                @foreach ($prodotto['artists'] as $artist)
+                                    {{ $artist }},
+                                @endforeach
+                            </p>
+                        </li>
+                        <li>
+                            <span>Written by:</span>
+                            <p>
+                                @foreach ($prodotto['writers'] as $writer)
+                                    {{ $writer }},
+                                @endforeach
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="specs">
+                    <h3>Specs</h3>
+                    <ul>
+                        <li>
+                            <span>Art by:</span>
+                            <span>{{ $prodotto['series'] }}</span>
+                        </li>
+                        <li>
+                            <span>U.S. Price:</span>
+                            <span>{{ $prodotto['price'] }}</span>
+                        </li>
+                        <li>
+                            <span>On Sale Date:</span>
+                            <span>{{ $prodotto['sale_date'] }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
