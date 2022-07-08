837d1e8bb308594a68f481cc7f315ef55d6587f5
rubacodes <rubacodes@gmail.com>
2022-07-08 15:09:58 +0200

    create routes

--- resources/views/partials/card_product.blade.php
+++ resources/views/partials/card_product.blade.php
@@ -2,3 +2,5 @@
       <img src="@yield('thumbnail')" alt="@yield('titolo_comic')" />
       <h3> @yield('titolo_comic') </h3>
   </div>
+
+  {{-- non posso ciclare i template ma devo creare i componenti --}}
